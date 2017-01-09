19f5eca32b9e87c2a2765ae01ab2543c037aa358
Michael Diender <michael.diender@shinesolutions.com>
2017-01-09 12:15:10 +1100

    remove the create-topic and delete-topic scripts - topics are now create via the messaging stack.

--- shell/create-topic.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-aws sns create-topic \
-    --name aem-stack-asg-topic
--- shell/delete-topic.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-aws sns delete-topic \
-    --topic-arn arn:aws:sns:region:accountID:aem-stack-asg-topic
