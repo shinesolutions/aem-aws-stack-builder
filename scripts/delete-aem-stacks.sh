1b1d117ee5289104a7a290c25ffe53f5685e941c
Cliffano Subagio <cliffano@gmail.com>
2017-02-03 11:48:29 +1100

    Update sandpit AMIs.

--- scripts/delete-aem-stacks.sh
+++ scripts/delete-aem-stacks.sh
@@ -23,7 +23,7 @@ delete_multi_stacks() {
 }
 
 echo "Deleting $stack_prefix AEM stacks..."
-delete_single_stack "apps/dns-records"
+delete_single_stack "apps/dns-records" &
 delete_multi_stacks "apps/chaos-monkey apps/orchestrator apps/author-dispatcher apps/publish-dispatcher apps/publish apps/author"
 delete_multi_stacks "apps/messaging apps/security-groups"
 delete_single_stack "apps/stack-data"
