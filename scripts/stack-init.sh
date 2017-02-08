baa2548a6e0f7e952ac2b51c4f151bfbf9f77a13
Michael Diender <michael.diender@shinesolutions.com>
2017-02-08 16:25:12 +1100

    if custom-provisioner exists in stack bucket. download it and if it contains a custom-common.sh execute it.

--- scripts/stack-init.sh
+++ scripts/stack-init.sh
@@ -19,6 +19,19 @@ puppet --version
 python --version
 ruby --version
 
+if aws s3 ls "s3://${data_bucket_name}/${stack_prefix}/" | grep aem-aws-stack-custom-provisioner.tar.gz
+then
+
+    echo "Downloading AEM Stack Custom Provisioner..."
+    mkdir -p /opt/shinesolutions/aem-aws-stack-custom-provisioner/
+    aws s3 cp "s3://${data_bucket_name}/${stack_prefix}/aem-aws-stack-custom-provisioner.tar.gz" /opt/shinesolutions/aem-aws-stack-custom-provisioner/aem-aws-stack-custom-provisioner.tar.gz
+    cd /opt/shinesolutions/aem-aws-stack-custom-provisioner/
+    gunzip aem-aws-stack-custom-provisioner.tar.gz
+    tar -xvf aem-aws-stack-custom-provisioner.tar
+    rm aem-aws-stack-custom-provisioner.tar
+
+fi
+
 echo "Downloading AEM Stack Provisioner..."
 mkdir -p /opt/shinesolutions/aem-aws-stack-provisioner/
 aws s3 cp "s3://${data_bucket_name}/${stack_prefix}/aem-aws-stack-provisioner.tar.gz" /opt/shinesolutions/aem-aws-stack-provisioner/aem-aws-stack-provisioner.tar.gz
@@ -26,6 +39,15 @@ cd /opt/shinesolutions/aem-aws-stack-provisioner/
 gunzip aem-aws-stack-provisioner.tar.gz
 tar -xvf aem-aws-stack-provisioner.tar
 rm aem-aws-stack-provisioner.tar
+
+
+if -d "/opt/shinesolutions/aem-aws-stack-provisioner/" && -f "/opt/shinesolutions/aem-aws-stack-provisioner/custom-common.sh"; then
+
+    echo "Execute the custom provisioning script..."
+    cd /opt/shinesolutions/aem-aws-stack-provisioner/ && ./custom-common.sh
+
+fi
+
 
 echo "Applying common Puppet manifest for all components..."
 puppet apply --modulepath modules --hiera_config conf/hiera.yaml manifests/common.pp
@@ -41,3 +63,4 @@ puppet apply --modulepath modules --hiera_config conf/hiera.yaml "manifests/${co
 
 echo "Testing ${component} component using Serverspec..."
 cd test/serverspec && rake spec "SPEC=spec/${component}_spec.rb"
+
