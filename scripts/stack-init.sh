1184540d15ee64454a81d7f998fc9e699be4737d
James Sinclair <james.sinclair@shinesolutions.com>
2017-05-25 16:41:53 +1000

    Always create Facter 'facts.d' directory.

--- scripts/stack-init.sh
+++ scripts/stack-init.sh
@@ -74,10 +74,9 @@ if [ "$#" -eq 5 ]; then
   cp "${local_yaml_file}" "${local_yaml_path}"
 fi
 
-if [[ -d /opt/puppetlabs/facter/facts.d/ ]]; then
-  echo "Attempting to copy stack facts to Facter 'facts.d' directory."
-  aws s3 cp "s3://${data_bucket_name}/${stack_prefix}/stack-facts.txt" /opt/puppetlabs/facter/facts.d/stack-facts.txt
-fi
+echo "Attempting to copy stack facts to Facter 'facts.d' directory."
+mkdir -p /opt/puppetlabs/facts/facts.d
+aws s3 cp "s3://${data_bucket_name}/${stack_prefix}/stack-facts.txt" /opt/puppetlabs/facter/facts.d/stack-facts.txt
 
 export FACTER_data_bucket_name="${data_bucket_name}"
 export FACTER_stack_prefix="${stack_prefix}"
