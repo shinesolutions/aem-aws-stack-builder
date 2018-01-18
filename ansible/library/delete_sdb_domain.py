9359d76217955b7d23f1d27f2eead57112a55673
JASON LI <li.jason.aus@gmail.com>
2018-01-18 11:02:54 +1100

    Update delete_sdb_domain.py

--- ansible/library/delete_sdb_domain.py
+++ ansible/library/delete_sdb_domain.py
@@ -5,22 +5,29 @@
 from ansible.module_utils.basic import *
 import boto3
 
-def main():
+def delete_sdb_domain()
 
-  fields = {
-        "sdb_domain_name": {"required": True, "type": "str"},
-    }
+fields = dict(
+    sdb_domain_name=dict(required=True, type='str')
+  )
 
   module = AnsibleModule(argument_spec=fields)
-
+  
   client = boto3.client('sdb')
-
-  response = client.delete_domain(DomainName='module.params')  
+  
+  response = client.delete_domain(DomainName='module.params['sdb_domain_name']')  
     
   module.exit_json(changed = False, meta = response)
 
 
+def main():
+
+    delete_sdb_domain()  
+
 if __name__ == '__main__':
     main()
+
+
+
 
 
