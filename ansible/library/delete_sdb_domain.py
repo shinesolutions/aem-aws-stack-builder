81a6cd9bda8d2824e3efd226e4dacb0170b7a126
Cliffano Subagio <cliffano@gmail.com>
2018-02-07 00:36:08 +1100

    Add missing region to delete_sdb_domain ansible module.

--- ansible/library/delete_sdb_domain.py
+++ ansible/library/delete_sdb_domain.py
@@ -5,26 +5,27 @@
 from ansible.module_utils.basic import *
 import boto3
 
-def delete_sdb_domain(sdb_domain_name):
-    sdb = boto3.client('sdb')
-    sdb.delete_domain(DomainName=sdb_domain_name)
+def delete_sdb_domain(domain_name, region):
+    sdb = boto3.client('sdb', region_name=region)
+    sdb.delete_domain(DomainName=domain_name)
 
 def main():
 
     module = AnsibleModule(
       argument_spec = dict(
-        sdb_domain_name = dict(required=True, type='str')
+        domain_name = dict(required=True, type='str'),
+        region = dict(required=True, type='str')
       )
     )
 
-    delete_sdb_domain(module.params['sdb_domain_name'])
-    
+    delete_sdb_domain(module.params['domain_name'], module.params['region'])
+
     result = dict(
         changed=False,
         original_message='',
         message=''
     )
-    
+
     module.exit_json(**result)
 
 if __name__ == '__main__':
