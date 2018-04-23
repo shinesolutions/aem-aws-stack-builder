8229f4ea4b4c439921f7c4d4924908704eecd327
Cliffano Subagio <cliffano@gmail.com>
2018-04-24 09:02:11 +1000

    Add local integration test config generation.

--- test/integration-local/test-examples.sh
+++ test/integration-local/test-examples.sh
@@ -26,6 +26,7 @@ aws s3 cp "stage/aem-aws-stack-provisioner-${test_id}.tar.gz" s3://aem-stack-bui
 
 # create AEM environments
 cd ../aem-aws-stack-builder
+echo -e "library:\n  aem_aws_stack_provisioner_version: ${test_id}" > examples/user-config/common/zzz-test-integration-local.yaml
 make create-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=examples/user-config/aem-stack-manager/
 make config-examples-aem62-rhel7-full-set
 make create-full-set "stack_prefix=${test_id}-full-set" config_path=stage/user-config/aem62_sp1_cfp13-rhel7-full-set/
@@ -43,6 +44,6 @@ make test-full-set \
 
 # # delete AEM environments
 cd ../aem-aws-stack-builder
-# make delete-full-set "stack_prefix=${test_id}-full-set" config_path=stage/user-config/aem62_sp1_cfp13-rhel7-full-set/
-# make delete-consolidated "stack_prefix=${test_id}-consolidated" config_path=stage/user-config/aem62_sp1_cfp13-rhel7-consolidated/
-# make delete-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=examples/user-config/aem-stack-manager/
+make delete-full-set "stack_prefix=${test_id}-full-set" config_path=stage/user-config/aem62_sp1_cfp13-rhel7-full-set/
+make delete-consolidated "stack_prefix=${test_id}-consolidated" config_path=stage/user-config/aem62_sp1_cfp13-rhel7-consolidated/
+make delete-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=examples/user-config/aem-stack-manager/
