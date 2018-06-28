3a46858883d05b0e93133267fd9a65de1e55d05c
Cliffano Subagio <cliffano@gmail.com>
2018-06-28 23:54:15 +1000

    Add integration testing support using configured libraries.

--- test/integration/test-examples.sh
+++ test/integration/test-examples.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# This script is used for integration testing AEM environments using local
-# repos, which allows the user to decide whether to use master or a feature
-# branch that's being worked on for each of the repos.
-# The repositories must be located at the same directory level.
+# This script is used for integration testing AEM environments using
+# the configured libraries versions.
 # The AEM environments are created using examples user configurations.
 
 if [[ "$#" -lt 1 ]] || [[ "$#" -gt 3 ]]; then
@@ -17,37 +15,22 @@ aem_version=${2:-aem63}
 os_type=${3:-rhel7}
 s3_bucket=aem-stack-builder
 integration_config_file=examples/user-config/common/zzz-test.yaml
-workspace_dir=..
+aem_stack_manager_messenger_version=1.4.0
 
-# Download dependencies and run lint checks
-cd "${workspace_dir}/puppet-aem-resources" && make clean deps lint
-cd "${workspace_dir}/puppet-aem-curator" && make clean deps lint
-cd "${workspace_dir}/puppet-aem-orchestrator" && make clean deps lint
-cd "${workspace_dir}/aem-aws-stack-provisioner" && make clean deps lint
-cd "${workspace_dir}/aem-stack-manager-messenger" && make clean deps lint
-cd "${workspace_dir}/aem-aws-stack-builder" && make clean deps lint
-
-# Build AEM AWS Stack Provisioner and upload it to S3
-cd "${workspace_dir}/aem-aws-stack-provisioner"
-rm -rf modules/aem_resources/*
-rm -rf modules/aem_curator/*
-cp -R ${workspace_dir}/puppet-aem-resources/* modules/aem_resources/
-cp -R ${workspace_dir}/puppet-aem-curator/* modules/aem_curator/
-cp -R ${workspace_dir}/puppet-aem-orchestrator/* modules/aem_orchestrator/
-make package "version=${test_id}"
-aws s3 cp "stage/aem-aws-stack-provisioner-${test_id}.tar.gz" "s3://${s3_bucket}/library/"
+# Download Stack Manager Messenger
+wget "https://github.com/shinesolutions/aem-stack-manager-messenger/releases/download/${aem_stack_manager_messenger_version}/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory-prefix=stage
+mkdir -p "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
+tar -xvzf "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
 
 # Create AEM environments: a Stack Manager, an AEM Consolidated, and an AEM Full-Set
-cd "${workspace_dir}/aem-aws-stack-builder"
 rm -f "${integration_config_file}"
-echo -e "library:\n  aem_aws_stack_provisioner_version: ${test_id}" > "${integration_config_file}"
-echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-stack-manager" >> "${integration_config_file}"
+echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-stack-manager" > "${integration_config_file}"
 make config-examples-aem-stack-manager && make create-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=stage/user-config/aem-stack-manager/
 make "config-examples-${aem_version}-${os_type}-full-set" && make create-full-set "stack_prefix=${test_id}-full-set" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/"
 make "config-examples-${aem_version}-${os_type}-consolidated" && make create-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/"
 
 # Run integration tests via Stack Manager Messenger
-cd "${workspace_dir}/aem-stack-manager-messenger"
+cd "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
 make test-consolidated \
   "stack_prefix=${test_id}-stack-manager" \
   "target_aem_stack_prefix=${test_id}-consolidated"
@@ -56,7 +39,7 @@ make test-full-set \
   "target_aem_stack_prefix=${test_id}-full-set"
 
 # Delete all created AEM environments
-cd "${workspace_dir}/aem-aws-stack-builder"
+cd ../../
 (make "config-examples-${aem_version}-${os_type}-full-set" && make delete-full-set "stack_prefix=${test_id}-full-set" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/") &
 (make "config-examples-${aem_version}-${os_type}-consolidated" && make delete-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/") &
 (make config-examples-aem-stack-manager && make delete-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=stage/user-config/aem-stack-manager/) &
