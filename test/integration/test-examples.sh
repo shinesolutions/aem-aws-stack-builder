f48ec9c36a57be54caa4fdb58e3fc098932fe1e7
Cliffano Subagio <cliffano@gmail.com>
2018-07-13 12:15:57 +1000

    Ensure workspace dir before running commands.

--- test/integration/test-examples.sh
+++ test/integration/test-examples.sh
@@ -16,6 +16,7 @@ os_type=${3:-rhel7}
 integration_config_file=examples/user-config/common/zzz-test.yaml
 aem_stack_manager_messenger_version=1.4.1
 aem_test_suite_version=0.9.3
+workspace_dir=$(pwd)
 
 # Create AEM environments: a Stack Manager, an AEM Consolidated, and an AEM Full-Set
 rm -f "${integration_config_file}"
@@ -25,12 +26,13 @@ make "config-examples-${aem_version}-${os_type}-full-set" && make create-full-se
 make "config-examples-${aem_version}-${os_type}-consolidated" && make create-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/"
 
 # Download AEM Test Suite
+cd "${workspace_dir}"
 wget "https://github.com/shinesolutions/aem-test-suite/releases/download/${aem_test_suite_version}/aem-test-suite-${aem_test_suite_version}.tar.gz" --directory-prefix=stage
 mkdir -p "stage/aem-test-suite-${aem_test_suite_version}"
 tar -xvzf "stage/aem-test-suite-${aem_test_suite_version}.tar.gz" --directory "stage/aem-test-suite-${aem_test_suite_version}"
 
 # Run AEM Test Suite integration tests
-cd "stage/aem-test-suite-${aem_test_suite_version}"
+cd "${workspace_dir}/stage/aem-test-suite-${aem_test_suite_version}"
 make test-readiness-full-set "stack_prefix=${test_id}-full-set" config_path=conf/
 make test-acceptance-full-set "stack_prefix=${test_id}-full-set" config_path=conf/
 make test-recovery-full-set "stack_prefix=${test_id}-full-set" config_path=conf/
@@ -38,12 +40,13 @@ make test-recovery-full-set "stack_prefix=${test_id}-full-set" config_path=conf/
 # make test-security config_path=conf/
 
 # Run Stack Manager Messenger integration tests
+cd "${workspace_dir}"
 wget "https://github.com/shinesolutions/aem-stack-manager-messenger/releases/download/${aem_stack_manager_messenger_version}/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory-prefix=stage
 mkdir -p "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
 tar -xvzf "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
 
 # Run integration tests via Stack Manager Messenger
-cd "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
+cd "${workspace_dir}/stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
 make test-consolidated \
   "stack_prefix=${test_id}-stack-manager" \
   "target_aem_stack_prefix=${test_id}-consolidated"
@@ -52,7 +55,7 @@ make test-full-set \
   "target_aem_stack_prefix=${test_id}-full-set"
 
 # Delete all created AEM environments
-cd ../../
+cd "${workspace_dir}"
 (make "config-examples-${aem_version}-${os_type}-full-set" && make delete-full-set "stack_prefix=${test_id}-full-set" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/") &
 (make "config-examples-${aem_version}-${os_type}-consolidated" && make delete-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/") &
 (make config-examples-aem-stack-manager && make delete-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=stage/user-config/aem-stack-manager/) &
