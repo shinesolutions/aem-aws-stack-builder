97f388926da5b1427c49763361ac1ec4225db5c1
Cliffano Subagio <cliffano@gmail.com>
2018-10-11 07:03:49 +1100

    Temp test without environment provisioning.

--- test/integration/test-examples.sh
+++ test/integration/test-examples.sh
@@ -18,35 +18,35 @@ aem_stack_manager_messenger_version=1.5.6
 aem_test_suite_version=0.9.8
 workspace_dir=$(pwd)
 
-# Create AEM environments: a Stack Manager, an AEM Consolidated, and an AEM Full-Set
-rm -f "${integration_config_file}"
-echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-stack-manager" > "${integration_config_file}"
-make config-examples-aem-stack-manager && make create-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=stage/user-config/aem-stack-manager/
-make "config-examples-${aem_version}-${os_type}-full-set" && make create-full-set "stack_prefix=${test_id}-full-set" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/"
-make "config-examples-${aem_version}-${os_type}-consolidated" && make create-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/"
-
-# Download Stack Manager Messenger and resolve dependencies
-cd "${workspace_dir}"
-wget "https://github.com/shinesolutions/aem-stack-manager-messenger/releases/download/${aem_stack_manager_messenger_version}/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory-prefix=stage
-mkdir -p "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
-tar -xvzf "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
-cd "${workspace_dir}/stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
-make deps
-
-# Run Stack Manager Messenger integration tests
-cd "${workspace_dir}"
-wget "https://github.com/shinesolutions/aem-stack-manager-messenger/releases/download/${aem_stack_manager_messenger_version}/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory-prefix=stage
-mkdir -p "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
-tar -xvzf "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
-
-# Run integration tests via Stack Manager Messenger
-cd "${workspace_dir}/stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
-make test-consolidated \
-  "stack_prefix=${test_id}-stack-manager" \
-  "target_aem_stack_prefix=${test_id}-consolidated"
-make test-full-set \
-  "stack_prefix=${test_id}-stack-manager" \
-  "target_aem_stack_prefix=${test_id}-full-set"
+# # Create AEM environments: a Stack Manager, an AEM Consolidated, and an AEM Full-Set
+# rm -f "${integration_config_file}"
+# echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-stack-manager" > "${integration_config_file}"
+# make config-examples-aem-stack-manager && make create-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=stage/user-config/aem-stack-manager/
+# make "config-examples-${aem_version}-${os_type}-full-set" && make create-full-set "stack_prefix=${test_id}-full-set" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/"
+# make "config-examples-${aem_version}-${os_type}-consolidated" && make create-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/"
+# 
+# # Download Stack Manager Messenger and resolve dependencies
+# cd "${workspace_dir}"
+# wget "https://github.com/shinesolutions/aem-stack-manager-messenger/releases/download/${aem_stack_manager_messenger_version}/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory-prefix=stage
+# mkdir -p "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
+# tar -xvzf "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
+# cd "${workspace_dir}/stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
+# make deps
+#
+# # Run Stack Manager Messenger integration tests
+# cd "${workspace_dir}"
+# wget "https://github.com/shinesolutions/aem-stack-manager-messenger/releases/download/${aem_stack_manager_messenger_version}/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory-prefix=stage
+# mkdir -p "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
+# tar -xvzf "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}.tar.gz" --directory "stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
+#
+# # Run integration tests via Stack Manager Messenger
+# cd "${workspace_dir}/stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
+# make test-consolidated \
+#   "stack_prefix=${test_id}-stack-manager" \
+#   "target_aem_stack_prefix=${test_id}-consolidated"
+# make test-full-set \
+#   "stack_prefix=${test_id}-stack-manager" \
+#   "target_aem_stack_prefix=${test_id}-full-set"
 
 # Download AEM Test Suite and resolve dependencies
 cd "${workspace_dir}"
