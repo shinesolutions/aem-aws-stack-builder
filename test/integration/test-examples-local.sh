0bcaaa329557d5654ea7c003a8e433bd2463f2ba
Cliffano Subagio <cliffano@gmail.com>
2018-10-21 01:05:54 +1100

    Shorten architecture type in integration test's stack prefixes to avoid hitting max resource names (often 64).

--- test/integration/test-examples-local.sh
+++ test/integration/test-examples-local.sh
@@ -49,31 +49,31 @@ aws s3 cp "stage/aem-stack-manager-cloud-${test_id}.zip" "s3://${s3_bucket}/libr
 cd "${workspace_dir}/aem-aws-stack-builder"
 rm -f "${integration_config_file}"
 echo -e "library:\n  aem_aws_stack_provisioner_version: ${test_id}\n  aem_stack_manager_version: ${test_id}" > "${integration_config_file}"
-echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-stack-manager" >> "${integration_config_file}"
-make config-examples-aem-stack-manager && make create-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=stage/user-config/aem-stack-manager/
-make "config-examples-${aem_version}-${os_type}-full-set" && make create-full-set "stack_prefix=${test_id}-full-set" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/"
-make "config-examples-${aem_version}-${os_type}-consolidated" && make create-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/"
+echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-sm" >> "${integration_config_file}"
+make config-examples-aem-stack-manager && make create-stack-manager "stack_prefix=${test_id}-sm" config_path=stage/user-config/aem-stack-manager/
+make "config-examples-${aem_version}-${os_type}-full-set" && make create-full-set "stack_prefix=${test_id}-fs" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/"
+make "config-examples-${aem_version}-${os_type}-consolidated" && make create-consolidated "stack_prefix=${test_id}-con" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/"
 
 # Run Stack Manager Messenger integration tests
 cd "${workspace_dir}/aem-stack-manager-messenger"
 make test-consolidated \
-  "stack_prefix=${test_id}-stack-manager" \
-  "target_aem_stack_prefix=${test_id}-consolidated"
+  "stack_prefix=${test_id}-sm" \
+  "target_aem_stack_prefix=${test_id}-con"
 make test-full-set \
-  "stack_prefix=${test_id}-stack-manager" \
-  "target_aem_stack_prefix=${test_id}-full-set"
+  "stack_prefix=${test_id}-sm" \
+  "target_aem_stack_prefix=${test_id}-fs"
 
 # Run AEM Test Suite integration tests
 cd "${workspace_dir}/aem-test-suite"
-make test-readiness-full-set "stack_prefix=${test_id}-full-set" config_path=conf/
-make test-acceptance-full-set "stack_prefix=${test_id}-full-set" config_path=conf/
-make test-recovery-full-set "stack_prefix=${test_id}-full-set" config_path=conf/
+make test-readiness-full-set "stack_prefix=${test_id}-fs" config_path=conf/
+make test-acceptance-full-set "stack_prefix=${test_id}-fs" config_path=conf/
+make test-recovery-full-set "stack_prefix=${test_id}-fs" config_path=conf/
 # placeholder security test for now, TODO: retrieve author, publish, and publish_dispatcher hosts
 # make test-security config_path=conf/
 
 # Delete all created AEM environments
 cd "${workspace_dir}/aem-aws-stack-builder"
-(make "config-examples-${aem_version}-${os_type}-full-set" && make delete-full-set "stack_prefix=${test_id}-full-set" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/") &
-(make "config-examples-${aem_version}-${os_type}-consolidated" && make delete-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/") &
-(make config-examples-aem-stack-manager && make delete-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=stage/user-config/aem-stack-manager/) &
+(make "config-examples-${aem_version}-${os_type}-full-set" && make delete-full-set "stack_prefix=${test_id}-fs" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/") &
+(make "config-examples-${aem_version}-${os_type}-consolidated" && make delete-consolidated "stack_prefix=${test_id}-con" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/") &
+(make config-examples-aem-stack-manager && make delete-stack-manager "stack_prefix=${test_id}-sm" config_path=stage/user-config/aem-stack-manager/) &
 wait
--- test/integration/test-examples.sh
+++ test/integration/test-examples.sh
@@ -20,10 +20,10 @@ workspace_dir=$(pwd)
 
 # Create AEM environments: a Stack Manager, an AEM Consolidated, and an AEM Full-Set
 rm -f "${integration_config_file}"
-echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-stack-manager" > "${integration_config_file}"
-make config-examples-aem-stack-manager && make create-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=stage/user-config/aem-stack-manager/
-make "config-examples-${aem_version}-${os_type}-full-set" && make create-full-set "stack_prefix=${test_id}-full-set" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/"
-make "config-examples-${aem_version}-${os_type}-consolidated" && make create-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/"
+echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-sm" > "${integration_config_file}"
+make config-examples-aem-stack-manager && make create-stack-manager "stack_prefix=${test_id}-sm" config_path=stage/user-config/aem-stack-manager/
+make "config-examples-${aem_version}-${os_type}-full-set" && make create-full-set "stack_prefix=${test_id}-fs" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/"
+make "config-examples-${aem_version}-${os_type}-consolidated" && make create-consolidated "stack_prefix=${test_id}-con" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/"
 
 # Download Stack Manager Messenger and resolve dependencies
 cd "${workspace_dir}"
@@ -36,11 +36,11 @@ make deps
 # Run Stack Manager Messenger integration tests
 cd "${workspace_dir}/stage/aem-stack-manager-messenger-${aem_stack_manager_messenger_version}"
 make test-consolidated \
-  "stack_prefix=${test_id}-stack-manager" \
-  "target_aem_stack_prefix=${test_id}-consolidated"
+  "stack_prefix=${test_id}-sm" \
+  "target_aem_stack_prefix=${test_id}-con"
 make test-full-set \
-  "stack_prefix=${test_id}-stack-manager" \
-  "target_aem_stack_prefix=${test_id}-full-set"
+  "stack_prefix=${test_id}-sm" \
+  "target_aem_stack_prefix=${test_id}-fs"
 
 # Download AEM Test Suite and resolve dependencies
 cd "${workspace_dir}"
@@ -62,8 +62,20 @@ make test-readiness-full-set "stack_prefix=${test_id}-full-set" config_path=conf
 make test-acceptance-full-set "stack_prefix=${test_id}-full-set" config_path=conf/
 # TODO: temporarily disable recovery testing to allow CodeBuild to pass
 #       will re-enable when we've improved buildspec for CodeBuild, and this script
-#       can return to be used by developers only
-# make test-recovery-full-set "stack_prefix=${test_id}-full-set" config_path=conf/
+#       can return to be used by developers only, or aws-sdk is upgraded to 3.x.x
+# # Download AEM Test Suite and resolve dependencies
+# cd "${workspace_dir}"
+# wget "https://github.com/shinesolutions/aem-test-suite/releases/download/${aem_test_suite_version}/aem-test-suite-${aem_test_suite_version}.tar.gz" --directory-prefix=stage
+# mkdir -p "stage/aem-test-suite-${aem_test_suite_version}"
+# tar -xvzf "stage/aem-test-suite-${aem_test_suite_version}.tar.gz" --directory "stage/aem-test-suite-${aem_test_suite_version}"
+# cd "${workspace_dir}/stage/aem-test-suite-${aem_test_suite_version}"
+# make deps
+#
+# # Run AEM Test Suite integration tests
+# cd "${workspace_dir}/stage/aem-test-suite-${aem_test_suite_version}"
+# make test-readiness-full-set "stack_prefix=${test_id}-fs" config_path=conf/
+# make test-acceptance-full-set "stack_prefix=${test_id}-fs" config_path=conf/
+# make test-recovery-full-set "stack_prefix=${test_id}-fs" config_path=conf/
 
 # placeholder security test for now, TODO: retrieve author, publish, and publish_dispatcher hosts
 # make test-security config_path=conf/
@@ -75,7 +87,7 @@ gem uninstall --all --force aws-sdk
 # Delete all created AEM environments
 cd "${workspace_dir}"
 make deps
-(make "config-examples-${aem_version}-${os_type}-full-set" && make delete-full-set "stack_prefix=${test_id}-full-set" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/") &
-(make "config-examples-${aem_version}-${os_type}-consolidated" && make delete-consolidated "stack_prefix=${test_id}-consolidated" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/") &
-(make config-examples-aem-stack-manager && make delete-stack-manager "stack_prefix=${test_id}-stack-manager" config_path=stage/user-config/aem-stack-manager/) &
+(make "config-examples-${aem_version}-${os_type}-full-set" && make delete-full-set "stack_prefix=${test_id}-fs" "config_path=stage/user-config/${aem_version}-${os_type}-full-set/") &
+(make "config-examples-${aem_version}-${os_type}-consolidated" && make delete-consolidated "stack_prefix=${test_id}-con" "config_path=stage/user-config/${aem_version}-${os_type}-consolidated/") &
+(make config-examples-aem-stack-manager && make delete-stack-manager "stack_prefix=${test_id}-sm" config_path=stage/user-config/aem-stack-manager/) &
 wait
