85c1fefa26ff67f10fc65784ff28b2a855e6e45c
Cliffano Subagio <cliffano@gmail.com>
2019-08-11 17:50:51 +1000

    Revert accidental comment-out of integration tests.

--- test/integration/test-examples-local.sh
+++ test/integration/test-examples-local.sh
@@ -37,44 +37,44 @@ make create-aws-resources "stack_prefix=${test_id}-res" config_path=stage/user-c
 echo "Deleting AWS resources..."
 make delete-aws-resources "stack_prefix=${test_id}-res" config_path=stage/user-config/aws-resources-sandpit/
 
-# # Create integration test configuration
-# echo "Creating integration test configuration file..."
-# rm -f "${integration_test_config_file}"
-# echo -e "library:\n  aem_aws_stack_provisioner_version: ${test_id}\n  aem_stack_manager_version: ${test_id}" > "${integration_test_config_file}"
-# echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-sm" >> "${integration_test_config_file}"
-#
-# # Create AEM Stack Manager environment
-# echo "Creating AEM Stack Manager environment..."
-# make create-stack-manager "stack_prefix=${test_id}-sm" config_path=stage/user-config/aem-stack-manager-sandpit/
-#
-# # Create, test, and delete AEM Consolidated environment
-# cp "${integration_test_config_file}" "stage/user-config/aem-consolidated-${os_type}-${aem_version}/"
-# echo "Creating AEM Consolidated environment..."
-# cp -R stage/descriptors/consolidated/* stage/
-# make create-consolidated "stack_prefix=${test_id}-con" "config_path=stage/user-config/aem-consolidated-${os_type}-${aem_version}/"
-# echo "Testing AEM Consolidated environment with AEM Stack Manager Messenger events..."
-# cd stage/aem-stack-manager-messenger/ && make test-consolidated "stack_prefix=${test_id}-sm" "target_aem_stack_prefix=${test_id}-con" && cd ../../
-# echo "Deleting AEM Consolidated environment..."
-# make delete-consolidated "stack_prefix=${test_id}-con" "config_path=stage/user-config/aem-consolidated-${os_type}-${aem_version}/"
-#
-# # Create, test, and delete AEM Full-Set environment
-# cp "${integration_test_config_file}" "stage/user-config/aem-full-set-${os_type}-${aem_version}/"
-# echo "Creating AEM Full-Set environment..."
-# cp -R stage/descriptors/full-set/* stage/
-# make create-full-set "stack_prefix=${test_id}-fs" "config_path=stage/user-config/aem-full-set-${os_type}-${aem_version}/"
-# echo "Testing AEM Full-Set environment with AEM Stack Manager Messenger events..."
-# cd stage/aem-stack-manager-messenger/ && make test-full-set "stack_prefix=${test_id}-sm" "target_aem_stack_prefix=${test_id}-fs" && cd ../../
-# # TODO: temporarily disable aem-test-suite testing to allow CodeBuild to pass
-# #       will re-enable when InSpec already upgrades aws-sdk sub dependency to version 3.x
-# # echo "Testing AEM Full-Set environment readiness with AEM Test Suite..."
-# # cd /stage/aem-test-suite/ && make test-readiness-full-set "stack_prefix=${test_id}-fs" config_path=conf/ && cd ../../
-# # echo "Testing AEM Full-Set environment acceptance with AEM Test Suite..."
-# # cd /stage/aem-test-suite/ && make test-acceptance-full-set "stack_prefix=${test_id}-fs" config_path=conf/ && cd ../../
-# # echo "Testing AEM Full-Set environment recovery with AEM Test Suite..."
-# # cd /stage/aem-test-suite/ && make test-recovery-full-set "stack_prefix=${test_id}-fs" config_path=conf/ && cd ../../
-# echo "Deleting AEM Full-Set environment..."
-# make delete-full-set "stack_prefix=${test_id}-fs" "config_path=stage/user-config/aem-full-set-${os_type}-${aem_version}/"
-#
-# # Delete AEM Stack Manager
-# echo "Deleting AEM Stack Manager environment..."
-# make delete-stack-manager "stack_prefix=${test_id}-sm" config_path=stage/user-config/aem-stack-manager-sandpit/
+# Create integration test configuration
+echo "Creating integration test configuration file..."
+rm -f "${integration_test_config_file}"
+echo -e "library:\n  aem_aws_stack_provisioner_version: ${test_id}\n  aem_stack_manager_version: ${test_id}" > "${integration_test_config_file}"
+echo -e "scheduled_jobs:\n  aem_orchestrator:\n    stack_manager_pair:\n        stack_prefix: ${test_id}-sm" >> "${integration_test_config_file}"
+
+# Create AEM Stack Manager environment
+echo "Creating AEM Stack Manager environment..."
+make create-stack-manager "stack_prefix=${test_id}-sm" config_path=stage/user-config/aem-stack-manager-sandpit/
+
+# Create, test, and delete AEM Consolidated environment
+cp "${integration_test_config_file}" "stage/user-config/aem-consolidated-${os_type}-${aem_version}/"
+echo "Creating AEM Consolidated environment..."
+cp -R stage/descriptors/consolidated/* stage/
+make create-consolidated "stack_prefix=${test_id}-con" "config_path=stage/user-config/aem-consolidated-${os_type}-${aem_version}/"
+echo "Testing AEM Consolidated environment with AEM Stack Manager Messenger events..."
+cd stage/aem-stack-manager-messenger/ && make test-consolidated "stack_prefix=${test_id}-sm" "target_aem_stack_prefix=${test_id}-con" && cd ../../
+echo "Deleting AEM Consolidated environment..."
+make delete-consolidated "stack_prefix=${test_id}-con" "config_path=stage/user-config/aem-consolidated-${os_type}-${aem_version}/"
+
+# Create, test, and delete AEM Full-Set environment
+cp "${integration_test_config_file}" "stage/user-config/aem-full-set-${os_type}-${aem_version}/"
+echo "Creating AEM Full-Set environment..."
+cp -R stage/descriptors/full-set/* stage/
+make create-full-set "stack_prefix=${test_id}-fs" "config_path=stage/user-config/aem-full-set-${os_type}-${aem_version}/"
+echo "Testing AEM Full-Set environment with AEM Stack Manager Messenger events..."
+cd stage/aem-stack-manager-messenger/ && make test-full-set "stack_prefix=${test_id}-sm" "target_aem_stack_prefix=${test_id}-fs" && cd ../../
+# TODO: temporarily disable aem-test-suite testing to allow CodeBuild to pass
+#       will re-enable when InSpec already upgrades aws-sdk sub dependency to version 3.x
+# echo "Testing AEM Full-Set environment readiness with AEM Test Suite..."
+# cd /stage/aem-test-suite/ && make test-readiness-full-set "stack_prefix=${test_id}-fs" config_path=conf/ && cd ../../
+# echo "Testing AEM Full-Set environment acceptance with AEM Test Suite..."
+# cd /stage/aem-test-suite/ && make test-acceptance-full-set "stack_prefix=${test_id}-fs" config_path=conf/ && cd ../../
+# echo "Testing AEM Full-Set environment recovery with AEM Test Suite..."
+# cd /stage/aem-test-suite/ && make test-recovery-full-set "stack_prefix=${test_id}-fs" config_path=conf/ && cd ../../
+echo "Deleting AEM Full-Set environment..."
+make delete-full-set "stack_prefix=${test_id}-fs" "config_path=stage/user-config/aem-full-set-${os_type}-${aem_version}/"
+
+# Delete AEM Stack Manager
+echo "Deleting AEM Stack Manager environment..."
+make delete-stack-manager "stack_prefix=${test_id}-sm" config_path=stage/user-config/aem-stack-manager-sandpit/
